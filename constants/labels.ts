3abbc1f12143407056151016fcf90c7838230178
ThomasPerez91 <dialldialldiall@gmail.com>
2024-03-23 05:18:38 +0100

    chore: add constants + icons

---
+++ constants/labels.ts
@@ -0,0 +1,25 @@
+export const focusDesignLabels1 = [
+  "Game Length",
+  "Game Depth",
+  "Beginner Friendliness",
+  "Innovation",
+];
+export const focusDesignLabels2 = [
+  "Story",
+  "Character Design",
+  "Level Design",
+  "Mission Design",
+];
+
+export const designDirectionLabels = [
+  ["Core Gamers", "Casual Gamers"],
+  ["Non-violent", "Explicite Content"],
+  ["Easy", "Hard"],
+];
+
+export const designPriorityLabels = [
+  "Gameplay",
+  "Graphics",
+  "Sound",
+  "Technical",
+];
